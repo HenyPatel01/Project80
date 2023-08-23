153a11c674ca1ac9230132d2f28c25c468dbc028
Heny Patel <heny09112@gmail.com>
2023-08-22 22:07:50 -0400

    imported files and created stackNavigator

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from 'react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import DailyPicScreen from './screens/DailyPic';
+import SpaceCraftsScreen from './screens/SpaceCrafts';
+import StarMapScreen from './screens/StarMap';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="SpaceCrafts" component={SpaceCraftsScreen}/>
+        <Stack.Screen name="DailyPic" component={DailyPicScreen}/>
+        <Stack.Screen name="StarMap" component={StarMapScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
