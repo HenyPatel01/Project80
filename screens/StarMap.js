0920d7c7c929373b38060b1c10e35cea073a384b
Heny Patel <heny09112@gmail.com>
2023-09-11 19:17:46 -0400

    added styles and text input

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -24,7 +24,30 @@ export default class StarMapScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-
+                <SafeAreaView style={styles.droidSafeArea}/>
+                <View>
+                    <Text style={styles.titleText}>Star Map</Text>
+                    <TextInput
+                        style={{height: 40, borderColor: "gray", borderWidth: 1}}
+                        placeholder="Enter your latitude"
+                        placeholderTextColor = "#ffff#000000"
+                        onChangeText={(text)=>{
+                            this.setState({
+                                latitude: text
+                            })
+                        }}
+                    />
+                    <TextInput
+                        style={{height: 40, borderColor: "gray", borderWidth: 1}}
+                        placeholder="Enter your longitude"
+                        placeholderTextColor = "#ffff#000000"
+                        onChangeText={(text)=>{
+                            this.setState({
+                                longitude: text
+                            })
+                        }}
+                    />
+                </View>
             </View>
         )
     }
@@ -35,5 +58,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    titleText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "white",
+        justifyContent: "center",
+        alignContent: "center"
     }
 })
