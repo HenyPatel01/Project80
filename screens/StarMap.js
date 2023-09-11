ecbcc9badda2fe161059d8380a883090145e7eee
Heny Patel <heny09112@gmail.com>
2023-09-11 19:30:02 -0400

    fixed bugs

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -23,7 +23,7 @@ export default class StarMapScreen extends Component {
     }
     render() {
         const { longitude, latitude } = this.state;
-        const path = 'https://virtualsky.lco.global/embed/index.html?longitude=77.102493&latitude=28.704060&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true'
+        const path = 'https://virtualsky.lco.global/embed/index.html?longitude=${longitude}&latitude=${latitude}&constellations=true&constellationlabels=true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&showdate=false&showposition=false'
         return (
             <View style={styles.container}>
                 <SafeAreaView style={styles.droidSafeArea}/>
