67cc2cfd94d14662ef82a99555cf1add62582c87
Heny Patel <heny09112@gmail.com>
2023-09-13 17:06:55 -0400

    added apod function

--- screens/DailyPic.js
+++ screens/DailyPic.js
@@ -1,7 +1,30 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import axios from "axios";
 
 export default class DailyPicScreen extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            apod:{}
+        }
+    }
+
+    componentDidMount(){
+        this.getAPOD()
+    }
+
+    getAPOD = () => {
+        axios
+            .get("https://api.nasa.gov/planetary/apod?api_key=G5EFu6B4iIJZ77a9oPDdsOF0o4hbJYmchHXBOysH")
+            .then(response => {
+                this.setState({ apod: response.data })
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
+    }
+
     render() {
         return (
             <View
