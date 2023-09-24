c169adfb9e6bb46d86a9b73ec743a24b4b49e86c
Heny Patel <heny09112@gmail.com>
2023-09-24 16:44:08 -0400

    added the API and imported FlatList

--- screens/SpaceCrafts.js
+++ screens/SpaceCrafts.js
@@ -1,7 +1,42 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {
+    Text, 
+    View, 
+    StyleSheet, 
+    SafeAreaView, 
+    TouchableOpacity, 
+    Platform, 
+    StatusBar, 
+    FlatList,
+    ImageBackground, 
+    Image
+} from 'react-native';
+import axios from "axios";
 
 export default class SpaceCraftsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            aircrafts: []
+        }
+    }
+
+    componentDidMount() {
+        this.getData()
+
+    }
+
+    getData=()=>{
+        axios.get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/")
+                .then(response => {
+                    this.setState({ aircrafts: response.data.results })
+                    console.log(response.data.results)
+                })
+                .catch(error => {
+                    console.log(error.message)
+                })
+    }
+
     render() {
         return (
             <View
