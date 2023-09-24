6d76413bfe45f6f47d08eb0370059e808bf50d96
Heny Patel <heny09112@gmail.com>
2023-09-24 17:10:00 -0400

    showcased the data

--- screens/SpaceCrafts.js
+++ screens/SpaceCrafts.js
@@ -37,15 +37,39 @@ export default class SpaceCraftsScreen extends Component {
                 })
     }
 
+    renderItem = ({item}) =>{
+        return(
+            <View style={{borderWidth:1,justifyContent:"center",alignItems:"center",marginBottom:10,elevation:10}}>
+                <Image
+                source={{uri: item.agency.image_url}} 
+                style={{width:"100%",height:200,marginTop:15,marginBottom:15,marginRight:10}}></Image>
+
+            <Text style={{fontWeight:'bold',fontSize:20}}>{item.name}</Text>
+            <Text style={{color:'#696969'}}>{item.agency.name}</Text>
+            <Text>DESCRIPTION</Text>
+            <Text style={{color:'#A9A9A9',marginLeft:10,marginRight:10}}>{item.agency.description}</Text>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View
                 style={{
                     flex:1,
-                    justifyContent: "center",
+                    justifyContent:"center",
                     alignItems: "center"
                 }}>
-                <Text>Space Crafts Screen</Text>
+                <View styles={{flex:0.25}}>
+                    <Text>Space Crafts</Text>
+                </View>
+                <View styles={{flex:0.75}}>
+                <FlatList
+                    keyExtractor={this.keyExtractor}
+                    data={this.state.aircrafts}
+                    renderItem={this.renderItem}    
+                />
+                </View>
             </View>
         )
     }
